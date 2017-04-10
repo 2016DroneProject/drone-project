3f42c5be5ad57c3119054b59c44907e5ebae0cd2
Sihyeon Kim <ori_0404@naver.com>
2017-04-10 14:03:57 +0900

    바다모드_몬스터 객체 정리 및 진주 슈팅

---
+++ 프로젝트/바다모드작업/Ocean_Monster/Assets/KTK_Laser_Effects_Volume2/Scripts/Button_BackGroundOnOff.cs
@@ -0,0 +1,33 @@
+﻿//======================================
+/*
+@autor ktk.kumamoto
+@date 2015.3.13 create
+@note Button_BackGroundOnOff
+*/
+//======================================
+
+
+using UnityEngine;
+using System.Collections;
+
+public class Button_BackGroundOnOff : MonoBehaviour {
+	
+	private bool isChecked = true;
+	public GameObject BG_Set;
+	private string BackGround_State = "BackGround Display:ON";
+	
+	void OnGUI()
+	{
+		Rect rect1 = new Rect(400, 0, 200, 30);
+		isChecked = GUI.Toggle(rect1, isChecked, BackGround_State);
+		if (BG_Set != null){
+			if (isChecked) {
+				BackGround_State = "BackGround Display:ON";
+				BG_Set.active = true;
+			} else {
+				BackGround_State = "BackGround Display:OFF";
+				BG_Set.active = false;
+			}
+		}
+	}
+}
---
+++ 프로젝트/바다모드작업/Ocean_Monster/Assets/Scripts/Gun/GunShootingPearl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunShootingPearl : MonoBehaviour {
+
+    public GameObject pearl;
+    public Transform shotPos;
+    public float shootRate;
+
+    private float nextShootRate;
+
+    void Update()
+    {
+        // 컨트롤러 연동하면 마우스 입력 버튼 bool 변수(컨트롤러에서 받아오는)로 변경
+        if (Input.GetMouseButton(0) && Time.time > nextShootRate)
+        {
+            ShootPearl();
+        }
+    }
+
+    void ShootPearl()
+    {
+        nextShootRate = Time.time + shootRate;
+        // 진주 발사 부분
+        Instantiate(pearl, shotPos.position, pearl.transform.rotation);
+    }
+}
